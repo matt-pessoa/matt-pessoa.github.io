a00e123bd5069940cbcaa309ffcac802aa986840
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-03-09 17:42:09 -0300

    adiciona projects ao li

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Link as LinkScroll } from "react-scroll";
 import "./css/Header.css";
 import homeIconClicked from "../images/home-icon-clicked.svg";
 // import homeIconUnclicked from "../images/home-icon-uncliked.svg";
@@ -10,30 +9,12 @@ function Header() {
 		<header>
 			<nav>
 				<Link to="/">
-					{/* <LinkScroll
-						activeClass="active"
-						to="landing"
-						spy={true}
-						smooth={true}
-						offset={0}
-						duration={700}
-					> */}
 					<img className="home-icon" src={homeIconClicked} alt="home" />
-					{/* </LinkScroll> */}
 				</Link>
 				<ul>
-					<Link to="/work">
-						{/* <LinkScroll
-							activeClass="active"
-							to="work-page"
-							spy={true}
-							smooth={true}
-							offset={0}
-							duration={700}
-						> */}
-						projects
-						{/* </LinkScroll> */}
-					</Link>
+					<li>
+						<Link to="/work">projects</Link>
+					</li>
 					<li>
 						<Link to="/about">about me</Link>
 					</li>
