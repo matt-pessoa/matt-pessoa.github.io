56171b22c780c7f82dd475d0882eb6f2ff0b21ba
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-03-09 17:51:20 -0300

    adiciona logica para colorir a pagina atual

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router";
 import "./css/Header.css";
 import homeIconClicked from "../images/home-icon-clicked.svg";
-// import homeIconUnclicked from "../images/home-icon-uncliked.svg";
+import homeIconUnclicked from "../images/home-icon-unclicked.svg";
 
 function Header() {
+	const history = useHistory();
+	const path = history.location.pathname;
+
+	function checkCurrentPage(page) {
+		if (path === page) {
+			return { color: "#44deb2" };
+		}
+		return { color: "##D3D1D1" };
+	}
+
 	return (
 		<header>
 			<nav>
 				<Link to="/">
-					<img className="home-icon" src={homeIconClicked} alt="home" />
+					<img
+						className="home-icon"
+						src={path === "/" ? homeIconClicked : homeIconUnclicked}
+						alt="home"
+					/>
 				</Link>
 				<ul>
 					<li>
-						<Link to="/work">projects</Link>
+						<Link to="/work" style={checkCurrentPage("/work")}>
+							projects
+						</Link>
 					</li>
 					<li>
-						<Link to="/about">about me</Link>
+						<Link to="/about" style={checkCurrentPage("/about")}>
+							about me
+						</Link>
 					</li>
 					<li>
-						<Link to="/contact">contact</Link>
+						<Link to="/contact" style={checkCurrentPage("/contact")}>
+							contact
+						</Link>
 					</li>
 				</ul>
 			</nav>
