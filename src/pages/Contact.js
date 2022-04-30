8e5ffe54654af6a0d8938c8c8b0576442a335552
matt-pessoa <matheuspessoafonseca@gmail.com>
2022-04-29 22:00:51 -0300

    feat: contact heading

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import "./css/Contact.css";
 
 function Contact() {
 	return (
 		<div>
 			<Header />
-			<h1>Contact</h1>
+			<section className="limiter-contact contact" id="contact-page">
+				<h1 className="contact-heading">&lt;contact&gt;</h1>
+			</section>
 		</div>
 	);
 }
